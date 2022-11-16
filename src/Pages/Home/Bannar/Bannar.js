0b3241861ebdd8dd6cba1636c57db5f24acb2175
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-11-16 09:58:46 +0600

    bannar

--- src/Pages/Home/Bannar/Bannar.js
+++ src/Pages/Home/Bannar/Bannar.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import chair from '../../../assets/images/chair.png'
 
 const Bannar = () => {
   return (
-    <div>Bannar</div>
+    <div className="hero  ">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src={chair} className=" w-1/2 rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">Your New Smile Starts Here!</h1>
+      <p className="py-6">Doctors are one of the most important people in society. They are the sole reason our society is taking healthy and positive steps towards progress because the health of the body and mind is the reason behind the good work people put up towards development.</p>
+      <button className="btn btn-primary bg-gradient-to-r from-cyan-500 to-blue-500">Get Started</button>
+    </div>
+  </div>
+</div>
   )
 }
 
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Bannar from '../Bannar/Bannar'
 
 const Home = () => {
   return (
-    <div className='mx-5'>Home</div>
+    <div className='mx-5'>
+        <Bannar/>
+    </div>
   )
 }
 
